9ffddcd330842c12ea19b39629c7a18a0fcda855
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-08-07 11:12:10 +0200

    creating message lang

---
+++ resources/lang/pt/messages.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    // Meta
+    'tagline' => 'Engenharia | Manutenção | Tecnologia | Peças',
+    'meta_description' => 'MainGDream fornece soluções completas em engenharia, manutenção, tecnologia e peças sobressalentes.',
+    'meta_keywords' => 'engenharia, manutenção, tecnologia, peças sobressalentes, Moçambique',
+    
+    // Navigation
+    'nav' => [
+        'home' => 'Início',
+        'about' => 'Sobre Nós',
+        'about_us' => 'Sobre a Empresa',
+        'mission' => 'Missão & Visão',
+        'team' => 'Nossa Equipe',
+        'services' => 'Serviços',
+        'all_services' => 'Todos os Serviços',
+        'engineering' => 'Engenharia',
+        'maintenance' => 'Manutenção',
+        'technology' => 'Tecnologia',
+        'spare_parts' => 'Peças Sobressalentes',
+        'projects' => 'Projetos',
+        'contact' => 'Contacto',
+    ],
+    
+    // Company Info
+    'company' => [
+        'mission' => 'Fornecer soluções completas e inteligentes em engenharia, manutenção, tecnologia e fornecimento de peças sobressalentes, garantindo performance, confiabilidade e inovação em todas as operações dos clientes.',
+        'vision' => 'Ser referência em soluções técnicas e tecnológicas a nível nacional, regional e internacional, destacando-se pela excelência, inovação e confiança dos nossos serviços.',
+    ],
+    
+    // Contact
+    'address' => 'Beira, Sofala, Moçambique',
+    'phone' => '+258 84 123 4567',
+    'email' => 'info@maingdream.co.mz',
+];
